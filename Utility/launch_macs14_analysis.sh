8d7c5bd0bd511862dbf67a1b652fa96b7a15ade7
Charles Joly Beauparlant <Charles.Joly-Beauparlant@crchul.ulaval.ca>
2013-04-19 13:16:13 -0400

    First version of the macs14 wrapper
    Signed-off-by: Charles Joly Beauparlant <Charles.Joly-Beauparlant@crchul.ulaval.ca>

---
+++ Utility/launch_macs14_analysis.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+treatment=$1
+control=$2
+dir_output=$3
+prefix=$(basename ${treatment%.*})
+
+mkdir -p $dir_output
+
+output=$dir_output/$prefix
+
+macs14 -t $treatment -c $control -f BAM -g hs -n $output
