cdfb10c5815845f435d55dc02f65dc33664e835c
Charles Joly Beauparlant <Charles.Joly-Beauparlant@crchul.ulaval.ca>
2013-04-26 17:22:41 -0400

    Added some verbosity to PICS scripts
    Signed-off-by: Charles Joly Beauparlant <Charles.Joly-Beauparlant@crchul.ulaval.ca>

--- Utility/PICS_ctrl.R
+++ Utility/PICS_ctrl.R
@@ -10,14 +10,19 @@ if (length(argv) == 3) {
 	prefix <- argv[3]
 
 	# Load the files
+	print(paste("Loading treatment data: ", treatment, "...", sep=""))
 	dataIP <- bam2gr(treatment)
+	print(paste("Loading control data: ", control, "...", sep=""))
 	dataCont <- bam2gr(control)
 
 	# Do the actual analysis
+	print("Starting segmentation of the data...")
 	seg<-segmentPICS(dataIP, dataC=dataCont, minReads=1)
+	print("Doing the actual PICS analysis...")
 	pics <- PICS(seg)
 
 	# Save the results
+	print("Saving results")
 	myFilter<-list(score=c(2,Inf),delta=c(50,300),se=c(0,50),sigmaSqF=c(0,22500),sigmaSqR=c(0,22500))
 	rdBed<-makeRangedDataOutput(pics,type="bed",filter=c(myFilter,list(score=c(1,Inf))))
 	bed <- paste(prefix, "_peaks.bed", sep="")
